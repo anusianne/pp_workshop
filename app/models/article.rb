884225b5b87dc8282e4f133c7411bb7daf633a5b
ann_zielinska <100295263+anusianne@users.noreply.github.com>
2024-06-07 18:54:13 +0000

    added image display

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,6 @@
 class Article < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 5, maximum: 30}
+    validates :body, presence: true, length: {minimum: 15, maximum: 3000}
+    validates :published, presence: true, inclusion: [true, false], allow_blank: true
+    has_one_attached :image
 end
