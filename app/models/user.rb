031608dadff66d3c8ed0f746d91e6fa58d638f55
ann_zielinska <100295263+anusianne@users.noreply.github.com>
2024-07-21 13:42:49 +0000

    added validations for email regex and tests for userspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :validatable, :timeoutable
+  # :confirmable, :lockable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :validatable, :timeoutable, :recoverable, :rememberable
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
   validates :username, presence: true
-  validates :email, presence: true
+  validates :email, presence: true,
+  uniqueness: { case_sensitive: false },
+  format: { with: VALID_EMAIL_REGEX }
   validates :password, presence: true
 
 end
+
+
+
+
+
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,6 +11,34 @@ RSpec.describe User, type: :model do
                 expect(subject).to_not be_valid
                 expect(subject.errors[:username]).to include("can't be blank")
             end
+            it "is invalid" do
+                expect(subject).to_not be_valid
+                expect(subject.errors[:username]).to include("can't be blank")
+              end
+            end
+        
+            context "when email is already taken" do
+              let!(:user1) { create(:user, email: "test@example.com") }
+              let(:user2) { build(:user, email: "test@example.com", password: "password", password_confirmation: "password") }
+        
+              it "is invalid" do
+                expect(user2).not_to be_valid
+                expect(user2.errors[:email]).to include("has already been taken")
+              end
+            end
+        
+            context "when email is invalid" do
+              let(:user) { build(:user, email: "plainaddress", password: "password", password_confirmation: "password") }
+        
+              it "is invalid" do
+                expect(user).not_to be_valid
+                expect(user.errors[:email]).to include("is invalid")
+              end
+            end
+          end
         end
-    end
-end
+        
+
+
+
+
